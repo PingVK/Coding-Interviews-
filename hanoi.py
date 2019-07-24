1516679e10dc7628a2f69876a460161702259866
PingVK <dshangping@gmail.com>
2019-07-24 17:06:50 +0800

    add hanoi problem

---
+++ hanoi.py
@@ -0,0 +1,10 @@
+def hanoi(n, x='x', y='y', z='z'):
+    if n == 1:
+        print(x, '-->', z)
+    else:
+        hanoi(n-1, x, z, y)  # 将前n-1个盘子从x移动到y上
+        print(x, '-->', z)  # 将最底下的盘子从x移动到z上
+        hanoi(n-1, y, x, z)  # 将y上的n-1个盘子移动到z上
+
+
+hanoi(5)
