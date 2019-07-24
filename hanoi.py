bd2a20543a43e4208c6db4d1c56422bda7c587dd
PingVK <dshangping@gmail.com>
2019-07-24 17:13:20 +0800

    modify hanoi.py

--- hanoi.py
+++ hanoi.py
@@ -1,9 +1,13 @@
+def move(a, b):
+    print('%s --> %s' % (a, b))
+
+
 def hanoi(n, x='x', y='y', z='z'):
     if n == 1:
-        print(x, '-->', z)
+        move(x, z)
     else:
         hanoi(n-1, x, z, y)  # 将前n-1个盘子从x移动到y上
-        print(x, '-->', z)  # 将最底下的盘子从x移动到z上
+        move(x, z)  # 将最底下的盘子从x移动到z上
         hanoi(n-1, y, x, z)  # 将y上的n-1个盘子移动到z上
 
 
